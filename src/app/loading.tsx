46fc20bd1c405928f918f6b6c55848933ccad744
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-05-18 22:59:09 +0600

    first commit

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import "../style/loding.css";
+
+const LoadingPage = () => {
+  return (
+    <div id="container">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingPage;
